b422fc719067bd3f5f37180cb660e4844fc7e2b5
Kay Gosho <ketsume0211@gmail.com>
2021-12-13 19:08:58 +0900

    enablet to save & record data

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,1 +1,83 @@
-console!.log('b')
+const recordedBlobs: Blob[] = []
+
+async function record() {
+  const media = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false,
+  })
+  const audioTrack = media.getAudioTracks()[0]
+  if (!audioTrack) {
+    throw new Error('Audio Track is null!')
+  }
+
+  const stream = new window.MediaStream([audioTrack])
+
+  const mimeType = `${audioTrack.kind}/webm`
+  const recorder = new MediaRecorder(stream, { mimeType })
+
+  recorder.ondataavailable = (event) => {
+    if (!event.data.size) {
+      return
+    }
+    const form = new FormData()
+    recordedBlobs.push(event.data)
+    // const superBuffer = new Blob(recordedBlobs, { type: 'audio/webm' })
+    // form.append('data', superBuffer)
+    fetch('/audio-stream/input', {
+      method: 'POST',
+      body: form,
+      // headers: {
+      //   'Content-Type': 'multipart/form-data',
+      // },
+    })
+      .then((res) => res.text())
+      .then(console.log)
+  }
+
+  // const audioConfig = AudioConfig.fromStreamInput(stream);
+  // const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+  // recognizer.startContinuousRecognitionAsync();
+  // recognizer.recognized = (_s, event) => {
+  //   if (event.result.text && event.result.text.length > 0) {
+  //     appendTranscription(event.result.text);
+  //   }
+  // };
+
+  recorder.start(1000)
+}
+
+function play() {
+  const superBuffer = new Blob(recordedBlobs, { type: 'audio/webm' })
+  document.querySelector('audio')!.src = window.URL.createObjectURL(superBuffer)
+}
+
+// function arrayBufferToString(buffer: Buffer) {
+//   const bufView = new Uint8Array(buffer)
+//   const length = bufView.length
+//
+//   let result = ''
+//   let addition = Math.pow(2, 8) - 1
+//
+//   for (let i = 0; i < length; i += addition) {
+//     if (i + addition > length) {
+//       addition = length - i
+//     }
+//     // @ts-ignore
+//     result += String.fromCharCode.apply(null, bufView.subarray(i, i + addition))
+//   }
+//
+//   return result
+// }
+
+async function listen() {
+  const sse = new EventSource('/audio-stream/output', {
+    withCredentials: false,
+  })
+  sse.onmessage = (data) => {
+    console.log(data)
+  }
+}
+
+document.getElementById('record')!.addEventListener('click', record)
+document.getElementById('listen')!.addEventListener('click', listen)
+document.getElementById('play')!.addEventListener('click', play)
--- server.ts
+++ server.ts
@@ -1,6 +1,14 @@
 import Fastify from 'fastify'
 import FastifyStatic from 'fastify-static'
 import path from 'path'
+import { FastifySSEPlugin } from 'fastify-sse-v2'
+import FastifyMultipart from 'fastify-multipart'
+import { on, EventEmitter } from 'events'
+import { pipeline } from 'stream'
+import fs from 'fs'
+import util from 'util'
+
+const pump = util.promisify(pipeline)
 
 const app = Fastify({ logger: true })
 
@@ -8,4 +16,52 @@ app.register(FastifyStatic, {
   root: path.resolve('./public'),
 })
 
+app.register(FastifySSEPlugin)
+app.register(FastifyMultipart)
+
+const ee = new EventEmitter()
+
+app.post<{ Body: { data: any } }>('/audio-stream/input', async (req, res) => {
+  const data = await req.file()
+  // console.log(file)
+  console.log(data)
+  // await pump(data.file, fs.createWriteStream('/tmp/record.webm'))
+
+  const buffer = await data.toBuffer()
+  fs.appendFile('/tmp/record.webm', buffer, (error) => {
+    if (error) {
+      console.error(error)
+      return
+    }
+  })
+
+  // ee.emit('append', req.body.data)
+  // ee.emit('append', '123')
+  res.send('ok')
+})
+
+app.get('/audio-stream/output', (_req, res) => {
+  res.sse(
+    (async function* () {
+      for await (const event of on(ee, 'append')) {
+        yield {
+          type: event.name,
+          data: JSON.stringify(event),
+        }
+      }
+    })(),
+  )
+})
+
 app.listen(9999)
+
+// function stringToArrayBuffer(str: string) {
+//   const buf = new ArrayBuffer(str.length)
+//   const bufView = new Uint8Array(buf)
+//
+//   for (let i = 0; i < str.length; i++) {
+//     bufView[i] = str.charCodeAt(i)
+//   }
+//
+//   return buf
+// }
