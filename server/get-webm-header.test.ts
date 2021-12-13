2183c1a76011d30834cd31f0dcbaf9d351e5845e
Kay Gosho <ketsume0211@gmail.com>
2021-12-14 00:29:52 +0900

    fix tsc

--- server/get-webm-header.test.ts
+++ server/get-webm-header.test.ts
@@ -1,5 +1,5 @@
 import { getWebmHeader } from './get-webm-header'
 
-getHeader('public/recordings/2021-12-13T13:11:41.248Z-rKPgEpzE.webm').then(
+getWebmHeader('public/recordings/2021-12-13T13:11:41.248Z-rKPgEpzE.webm').then(
   console.log,
 )
