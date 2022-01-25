a8a3b59db98be3438c600fbe030bced53ac3e11a
Kay Gosho <ketsume0211@gmail.com>
2022-01-26 01:49:12 +0900

    works without redis

--- server/file.ts
+++ server/file.ts
@@ -1,19 +1,27 @@
 import delay from 'delay'
 import IORedis from 'ioredis'
 import fs from 'fs/promises'
+import { EventEmitter } from 'events'
 
 const REDIS_URL = 'redis://127.0.0.1:46379'
 
-const publisher = new IORedis(REDIS_URL)
-const subscriber = new IORedis(REDIS_URL)
+const ee = new EventEmitter()
 
-subscriber.psubscribe('*')
-subscriber.on('pmessage', async (_, fileName, data) => {
-  await fs.appendFile(fileName, data)
+// const publisher = new IORedis(REDIS_URL)
+// const subscriber = new IORedis(REDIS_URL)
+
+// subscriber.psubscribe('*')
+// subscriber.on('pmessage', async (_, fileName, data) => {
+//   await fs.appendFile(fileName, Buffer.from(data))
+// })
+
+ee.on('message', async (fileName, data) => {
+  await fs.appendFile(fileName, Buffer.from(data))
 })
 
 export async function publish(fileName: string, data: Buffer) {
-  await publisher.publishBuffer(fileName, data)
+  // await publisher.publishBuffer(fileName, data)
+  await ee.emit('message', fileName, data)
 }
 
 if (module === require.main) {
