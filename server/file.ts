348bedbd3d0b426c9f9a6fa62c7e856ad79726a5
Kay Gosho <ketsume0211@gmail.com>
2022-01-26 02:00:35 +0900

    success using mqemitter-redis!

--- server/file.ts
+++ server/file.ts
@@ -1,27 +1,18 @@
 import delay from 'delay'
-import IORedis from 'ioredis'
 import fs from 'fs/promises'
-import { EventEmitter } from 'events'
+import MQEmitterRedis from 'mqemitter-redis'
 
-const REDIS_URL = 'redis://127.0.0.1:46379'
+const ee = MQEmitterRedis({
+  port: 46379,
+})
 
-const ee = new EventEmitter()
-
-// const publisher = new IORedis(REDIS_URL)
-// const subscriber = new IORedis(REDIS_URL)
-
-// subscriber.psubscribe('*')
-// subscriber.on('pmessage', async (_, fileName, data) => {
-//   await fs.appendFile(fileName, Buffer.from(data))
-// })
-
-ee.on('message', async (fileName, data) => {
-  await fs.appendFile(fileName, Buffer.from(data))
+ee.on('message', ({ fileName, data }, cb) => {
+  console.log(fileName)
+  fs.appendFile(fileName, data).then(cb)
 })
 
 export async function publish(fileName: string, data: Buffer) {
-  // await publisher.publishBuffer(fileName, data)
-  await ee.emit('message', fileName, data)
+  ee.emit({ topic: 'message', fileName, data })
 }
 
 if (module === require.main) {
